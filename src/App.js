fa88bb17d2dfc8ed802b06949576143315dad865
Vinicius Rodrigues <44007400+viniciuscsr@users.noreply.github.com>
2020-09-30 23:15:48 -0400

    create markComplete and delTodo function

--- src/App.js
+++ src/App.js
@@ -24,10 +24,31 @@ class App extends React.Component {
     ],
   };
 
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+
   render() {
     return (
       <div className='App'>
-        <Todos todos={this.state.todos} />
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
       </div>
     );
   }
