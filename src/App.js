8b5f67a08172a3fd4901cc878994558b1c4b1dd4
Vinicius Rodrigues <44007400+viniciuscsr@users.noreply.github.com>
2020-10-01 22:42:45 -0400

    router, addTodo component, about page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './Components/Layout/Header';
 import Todos from './Components/Todos';
+import AddTodo from './Components/AddTodo';
+import About from './Components/Pages/About';
+import { v4 as uuidv4 } from 'uuid';
 
 import './App.css';
 
@@ -41,15 +46,39 @@ class App extends React.Component {
     });
   };
 
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
   render() {
     return (
-      <div className='App'>
-        <Todos
-          todos={this.state.todos}
-          markComplete={this.markComplete}
-          delTodo={this.delTodo}
-        />
-      </div>
+      <Router>
+        <div className='App'>
+          <div className='Container'>
+            <Header />
+            <Route
+              exact
+              path='/'
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path='/about' component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
